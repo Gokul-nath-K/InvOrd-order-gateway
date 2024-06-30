3e6c73215a627d3072674255203d995fce2b8481
GOKUL NATH <gokulnathk003@gmail.com>
2024-07-01 00:44:23 +0530

    Order gateway created

---
+++ OrderGateway/src/test/java/com/quinbay/orderGateway/OrderGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinbay.orderGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
